b82d8aec48b6b8d7fc2cd39a594669b7799cd349
Pavel Máca <maca.pavel@gmail.com>
2017-01-06 09:33:26 +0100

    set production mode

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -17,6 +17,9 @@ $configurator->createRobotLoader()
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(__DIR__ . '/config/config.local.neon');
 
+//DEBUG mode
+$configurator->setDebugMode(FALSE);
+
 $container = $configurator->createContainer();
 
 return $container;
